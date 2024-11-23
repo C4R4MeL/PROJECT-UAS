c2589c2e097e788d9f586f6d2b4beff17f85ab8e
Muhammad Farhan Alafif <farhanfifel@gmail.com>
2024-11-23 18:08:38 +0700

    gsnti kode

--- main.c
+++ main.c
@@ -1,32 +1,34 @@
 #include "header.h"
 
 int main() {
+    int statusLogin;
+    char idMember[10];
     int pilihanLogin;
-    int statusLogin = 0;
-    char idMember[10];  // ID member untuk login
 
-    // Pilihan untuk login admin atau member
-    printf("Pilih login:\n");
-    printf("1. Login Admin\n");
-    printf("2. Login Member\n");
-    printf("Masukkan pilihan (1/2): ");
+    // Menampilkan pilihan login
+    printf("Login Sebagai:\n");
+    printf("1. Admin\n");
+    printf("2. Member\n");
+    printf("Pilih (1/2): ");
     scanf("%d", &pilihanLogin);
 
     if (pilihanLogin == 1) {
         // Login sebagai Admin
         statusLogin = loginAdmin();
-        if (statusLogin) {
-            printf("Login Admin berhasil. Anda dapat mengakses menu admin.\n");
+        if (statusLogin != 0) {
+            // Menu Admin
+            menuAdmin();
         } else {
-            printf("Login Admin gagal.\n");
+            printf("Login admin gagal.\n");
         }
     } else if (pilihanLogin == 2) {
         // Login sebagai Member
         statusLogin = loginMember(idMember);
-        if (statusLogin) {
-            printf("Login Member berhasil. ID Member: %s\n", idMember);
+        if (statusLogin != 0) {
+            // Menu Member
+            menuMember();
         } else {
-            printf("Login Member gagal.\n");
+            printf("Login member gagal.\n");
         }
     } else {
         printf("Pilihan tidak valid.\n");
