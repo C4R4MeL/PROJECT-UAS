f602db4ecdc671ce0001e8f80025e05bc9c0b3f1
Muhammad Farhan Alafif <farhanfifel@gmail.com>
2024-11-23 17:08:40 +0700

    sikit lagi

--- daftarbarang.c
+++ daftarbarang.c
@@ -1,19 +1,8 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-// Struktur Data Barang
-typedef struct {
-    char id[10];
-    char nama[50];
-    char kategori[30];
-    int stok;
-    float harga;
-} Barang;
+#include "header.h"
 
 // Fungsi untuk melihat daftar barang
 void lihatDaftarBarang() {
-    FILE *file = fopen("barang.txt", "r");
+    FILE *file = fopen(FILE_BARANG, "r");
     Barang barang;
 
     if (file == NULL) {
@@ -21,10 +10,11 @@ void lihatDaftarBarang() {
         return;
     }
 
+    printf("\n--- Daftar Barang ---\n");
     printf("ID\tNama\t\tKategori\tHarga\tStok\n");
-    printf("---------------------------------------------------\n");
+    printf("-----------------------------------------------\n");
 
-    // Membaca dan menampilkan data barang
+    // Membaca dan menampilkan data barang dari file
     while (fscanf(file, "%[^|]|%[^|]|%[^|]|%f|%d\n", barang.id, barang.nama, barang.kategori, &barang.harga, &barang.stok) != EOF) {
         printf("%s\t%s\t%s\t\t%.2f\t%d\n", barang.id, barang.nama, barang.kategori, barang.harga, barang.stok);
     }
@@ -32,8 +22,3 @@ void lihatDaftarBarang() {
     fclose(file);
 }
 
-int main() {
-    // Memanggil fungsi untuk melihat daftar barang
-    lihatDaftarBarang();
-    return 0;
-}
