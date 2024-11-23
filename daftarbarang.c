4bd05640f879bf670949ddb929d12e36b8327deb
Muhammad Farhan Alafif <farhanfifel@gmail.com>
2024-11-24 02:24:02 +0700

    Update daftarbarang.c

--- daftarbarang.c
+++ daftarbarang.c
@@ -14,12 +14,12 @@ void lihatDaftarBarang() {
     }
 
     printf("\n--- Daftar Barang ---\n");
-    printf("ID\tNama\t\tKategori\tHarga\tStok\n");
+    printf("ID\tNama\t\tKategori\tHarga\t\tStok\n");
     printf("-----------------------------------------------\n");
 
     // Membaca dan menampilkan data barang dari file
     while (fscanf(file, "%[^|]|%[^|]|%[^|]|%f|%d\n", barang.id, barang.nama, barang.kategori, &barang.harga, &barang.stok) != EOF) {
-        printf("%s\t%s\t%s\t\t%.2f\t%d\n", barang.id, barang.nama, barang.kategori, barang.harga, barang.stok);
+        printf("%s\t%s\t\t%s\t\t%.2f\t%d\n", barang.id, barang.nama, barang.kategori, barang.harga, barang.stok);
     }
 
     fclose(file);
